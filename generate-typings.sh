9061a441038e371bdc362eb86ad542f60a27c441
Andreas Reich <andreas@reich.name>
2016-08-30 11:31:42 +0300

    Removed unnecessary step from typings generation

--- generate-typings.sh
+++ generate-typings.sh
@@ -8,10 +8,8 @@ mkdir -p tmp
 `npm bin`/pbjs proto/POGOProtos.proto > tmp/POGOProtos.json
 
 # Create definition file from intermediate JSON
-`npm bin`/npp2ts -f tmp/POGOProtos.json >> pogo-protos.d.ts
+`npm bin`/npp2ts -f tmp/POGOProtos.json > pogo-protos.d.ts
 
 # Format definition file
 `npm bin`/tsfmt -r pogo-protos.d.ts
 
-# Clean up (remove empty lines)
-sed -e '/^[[:space:]]*$/d' -i '' pogo-protos.d.ts
