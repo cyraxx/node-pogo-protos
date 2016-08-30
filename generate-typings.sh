d549e645ed27c855a7a59595f0c1976ab35adfb6
Alexey Lizurchik <al.lizurchik@gmail.com>
2016-08-30 11:26:07 +0300

    Better TypeScript typings (#4)
    * typings support
    
    * replace proto2ts with npp2ts
    don't remove question marks
    
    * add Long and ByteBuffer to generated definitions
    update `npp2ts` module
    
    * exclude extra definitions
    update generator script

--- generate-typings.sh
+++ generate-typings.sh
@@ -7,15 +7,11 @@ mkdir -p tmp
 # Convert proto files into intermediate JSON
 `npm bin`/pbjs proto/POGOProtos.proto > tmp/POGOProtos.json
 
-# Add references to definition file
-echo "/// <reference path=\"bytebuffer.d.ts\" />" > pogo-protos.d.ts
-echo "/// <reference path=\"long.d.ts\" />" >> pogo-protos.d.ts
-
 # Create definition file from intermediate JSON
-`npm bin`/proto2ts -c false -u false -b false -f tmp/POGOProtos.json >> pogo-protos.d.ts
+`npm bin`/npp2ts -f tmp/POGOProtos.json >> pogo-protos.d.ts
 
 # Format definition file
 `npm bin`/tsfmt -r pogo-protos.d.ts
 
-# Clean up (remove question marks and empty lines)
-sed -e 's/\?//' -e '/^[[:space:]]*$/d' -i '' pogo-protos.d.ts
+# Clean up (remove empty lines)
+sed -e '/^[[:space:]]*$/d' -i '' pogo-protos.d.ts
